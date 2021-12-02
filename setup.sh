48f044daf6930345d4b6da319234110fe18f915c
ChallaHalla <sc5455@nyu.edu>
2021-12-02 15:40:04 -0500

    Add setup shell script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+WORKSPACE="$HOME/workspace"
+
+ln -s $WORKSPACE/.vimrc $HOME/.vimrc
+ln -s $WORKSPACE/.vim $HOME/.vim
+ln -s $WORKSPACE/.bashrc $HOME/.bashrc
+ln -s $WORKSPACE/.bash_profile $HOME/.bash_profile
