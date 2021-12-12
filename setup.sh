71c7bb5c9413c3333ad3fcb2d3fe612eb591436f
ChallaHalla <sc5455@nyu.edu>
2021-12-12 13:55:36 -0500

    update setup

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@ WORKSPACE="$HOME/dotfiles"
 ln -s $WORKSPACE/.vimrc $HOME/.vimrc
 ln -s $WORKSPACE/.vim $HOME/.vim
 ln -s $WORKSPACE/.bashrc $HOME/.bashrc
+ln -s $WORKSPACE/.zshrc $HOME/.zshrc
 ln -s $WORKSPACE/.bash_profile $HOME/.bash_profile
